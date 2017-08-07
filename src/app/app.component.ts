0902788f03532ce7a00a5da1a98b239a182f8dce
Bryan Tumbaco <bryandario1994@gmail.com>
2017-08-07 01:46:02 -0500

    parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,35 @@ import { TodoDataService } from './todo-data.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 export class AppComponent {
   newTodo: Todo = new Todo();
 
   constructor(private todoDataService: TodoDataService) {
-  	this.todoDataService = todoDataService;
   }
 
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
-  }
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
 
-  toggleTodoComplete(todo) {
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+    // rename from toggleTodoComplete
+  onToggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
+  // rename from removeTodo
+  onRemoveTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
+
+}
 
   get todos() {
     return this.todoDataService.getAllTodos();
